3c695cf24597681537975b06c8abb65eba731374
anassk01 <akabil9999@gmail.com>
2026-02-09 17:03:48 +0100

    refactor cotisation service pour utiliser constants

--- src/services/cotisation.service.js
+++ src/services/cotisation.service.js
@@ -1,21 +1,15 @@
-// taux de cotisations
-const TAUX_SALARIE = 0.0448;
-const TAUX_PATRONAL = 0.1601;
-const PLAFOND = 6000;
+import { TAUX_SALARIE, TAUX_PATRONAL, PLAFOND_COTISATION } from "../utils/constants.js";
 
-// fonction calculer cotisation salariale
 export const calculerCotisationSalariale = (salaire) => {
-    const salaireBase = Math.min(salaire, PLAFOND);
+    const salaireBase = Math.min(salaire, PLAFOND_COTISATION);
     return salaireBase * TAUX_SALARIE;
 };
 
-// calcul cotisation patronale
 export const calculerCotisationPatronale = (salaire) => {
-    const salaireBase = Math.min(salaire,PLAFOND);
+    const salaireBase = Math.min(salaire, PLAFOND_COTISATION);
     return salaireBase * TAUX_PATRONAL;
 };
 
-// calcul total
 export const calculerCotisationTotale = (salaire) => {
     return calculerCotisationSalariale(salaire) + calculerCotisationPatronale(salaire);
 };
