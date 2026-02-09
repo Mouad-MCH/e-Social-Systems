ba895dd722a17d3beb92dd5863d359c983d0c96c
anassk01 <akabil9999@gmail.com>
2026-02-09 17:05:42 +0100

    fix droits service calculs et utiliser constants

--- src/services/droits.service.js
+++ src/services/droits.service.js
@@ -1,6 +1,7 @@
 import { declarations } from "../data/declaration.js";
 import { assures } from "../data/assures.js";
 import { calculerCotisationTotale } from "./cotisation.service.js";
+import { JOURS_VALIDATION_PAR_MOIS, JOURS_MIN_ELIGIBILITE } from "../utils/constants.js";
 
 export const obtenirMoisDeclares = (assureId) => {
     const assure = assures.find(a => a.id === assureId);
@@ -19,17 +20,19 @@ export const obtenirCotisationsTotales = (assureId) => {
     return declarations
         .filter(d => d.employeurId === assure.employeurId)
         .reduce((total, decl) => {
-            const cotisation = calculerCotisationTotale(assure.salaireMensuel);
-            return total + cotisation;
+            const cotisationDeclaration = decl.salaries.reduce((sum, salaire) =>
+                sum + calculerCotisationTotale(salaire), 0
+            );
+            return total + cotisationDeclaration;
         }, 0);
 };
 
 export const verifierEligibilite = (assureId) => {
     const moisDeclares = obtenirMoisDeclares(assureId);
-    const joursValidation = moisDeclares * 26;
+    const joursValidation = moisDeclares * JOURS_VALIDATION_PAR_MOIS;
 
     return {
-        eligible: joursValidation >= 54,
+        eligible: joursValidation >= JOURS_MIN_ELIGIBILITE,
         joursValidation: joursValidation
     };
 };
@@ -44,7 +47,10 @@ export const consulterDroits = (assureId) => {
 
     const declarationsAssure = declarations.filter(d => d.employeurId === assure.employeurId);
     const salaireMoyen = declarationsAssure.length > 0
-        ? declarationsAssure.reduce((sum, d) => sum + assure.salaireMensuel, 0) / declarationsAssure.length
+        ? declarationsAssure.reduce((sum, d) => {
+            const totalSalaires = d.salaries.reduce((s, sal) => s + sal, 0);
+            return sum + totalSalaires;
+        }, 0) / (declarationsAssure.reduce((count, d) => count + d.salaries.length, 0))
         : assure.salaireMensuel;
 
     return {
