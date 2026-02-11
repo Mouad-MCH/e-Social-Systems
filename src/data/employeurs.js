17ce73b68f1360190860014ee7b46acdeb692584
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-02-11 11:21:55 +0100

    Changement des ID's de 38189394 à 1, 2...

--- src/data/employeurs.js
+++ src/data/employeurs.js
@@ -1,18 +1,5 @@
-
-
 export const employeurs = [
-    {
-        id: 1770288629315,
-        raisonSociale: 'mouad',
-        secteur: 'ecommerce',
-        assures: [1770289179448, 1770289179222]
-    },
-
-    {
-        id: 1770280009315,
-        raisonSociale: 'MCH',
-        secteur: 'IT',
-        assures: [1770280000222]
-    }
-
+  { id: 1, raisonSociale: "mouad", secteur: "ecommerce", assures: [1,2] },
+  { id: 2, raisonSociale: "MCH", secteur: "IT", assures: [3] },
+  { id: 3, raisonSociale: "XYZ Corp", secteur: "BTP", assures: [] }
 ];
